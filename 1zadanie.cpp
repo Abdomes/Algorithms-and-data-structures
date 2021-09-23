5d476df3b67e53068435d59cf68269f9b146781a
Abdomes <91058164+Abdomes@users.noreply.github.com>
2021-09-23 18:00:20 +0300

    Add files via upload

---
+++ 1zadanie.cpp
@@ -0,0 +1,35 @@
+//Найти, на сколько нулей оканчивается n! 
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+void NumZero(int &n)
+{
+	int c2 = 0, c5 = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		int chislo = i;
+		while (chislo % 5 == 0 || chislo % 2 == 0) 
+		{
+			if (chislo % 5 == 0) 
+			{
+				c5 += 1;
+				chislo /= 5;
+			}
+			if (chislo % 2 == 0)
+			{
+				c2 += 1;
+				chislo /= 2;
+			}
+		}
+	}
+	cout << min(c5, c2);
+}
+
+int main() 
+{
+	int n;
+	cin >> n;
+	assert(n <= 1000);
+	NumZero(n);
+}
