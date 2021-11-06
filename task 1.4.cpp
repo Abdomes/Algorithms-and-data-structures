2a992d2d95619a7fb45e55f373301c2d4a5652b2
Abdomes <91058164+Abdomes@users.noreply.github.com>
2021-11-06 12:47:56 +0500

    Add files via upload

---
+++ task 1.4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<assert.h>
+
+using namespace std;
+
+int NOD(int& n, int& A)
+{
+	for (int i = 2; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+		{
+			return A = n / i;
+		}
+	}
+	return A = 1;
+}
+
+int main()
+{
+	int n, A, B;
+	cin >> n;
+	assert(1 < n && n <= 10000000);
+	NOD(n, A);
+	B = n - A;
+	cout << A << " " << B;
+}
