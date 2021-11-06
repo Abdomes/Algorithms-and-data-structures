67ff0436309ad9f85cc14a46072d5b0efc1fc78a
Abdomes <91058164+Abdomes@users.noreply.github.com>
2021-11-06 13:01:28 +0500

    Add files via upload

---
+++ task 2.4.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+#include<assert.h>
+
+#include<vector>
+
+using namespace std;
+
+void queue(int& k, int& n, int* a)
+{
+	int c, nn;
+	nn = n; c = 0;
+	while (nn != 1)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (a[i] != 0) {
+				c += 1;
+			}
+			if (c == k)
+			{
+				a[i] = 0;
+				nn = nn - 1;
+				c = 0;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] == 5)
+		{
+			cout << i + 1;
+		}
+	}
+}
+
+int main()
+{
+	int n, k, nn;
+	cin >> n >> k;
+	assert(1 <= k && k <= n && n <= 10000);
+	int* a = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = 5;
+	}
+	queue(k, n, a);
+	delete[] a;
+}
