8403915ba29360ab2aa54f3819607993a350e217
Abdomes <91058164+Abdomes@users.noreply.github.com>
2021-11-08 17:04:02 +0500

    Update task 2.4.cpp

--- task 2.4.cpp
+++ task 2.4.cpp
@@ -6,42 +6,43 @@ using namespace std;
 
 void queue(int& k, int& n, int* a)
 {
-	int c, nn;
-	nn = n; c = 0;
-	while (nn != 1)
+    int c, nn;
+    nn = n; c = 0;
+    while (nn != 1)
+    {
+        for (int i = 0; i < n; i++)
 	{
-		for (int i = 0; i < n; i++)
-		{
-			if (a[i] != 0) {
-				c += 1;
-			}
-			if (c == k)
-			{
-				a[i] = 0;
-				nn = nn - 1;
-				c = 0;
-			}
-		}
+	    if (a[i] != 0) 
+	    {
+	        c += 1;
+	    }
+	    if (c == k)
+	    {
+		a[i] = 0;
+	        nn = nn - 1;
+		c = 0;
+	    }
 	}
-	for (int i = 0; i < n; i++)
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 5)
 	{
-		if (a[i] == 5)
-		{
-			cout << i + 1;
-		}
+	    cout << i + 1;
 	}
+    }
 }
 
 int main()
 {
-	int n, k, nn;
-	cin >> n >> k;
-	assert(1 <= k && k <= n && n <= 10000);
-	int* a = new int[n];
-	for (int i = 0; i < n; i++)
-	{
-		a[i] = 5;
-	}
-	queue(k, n, a);
-	delete[] a;
+    int n, k, nn;
+    cin >> n >> k;
+    assert(1 <= k && k <= n && n <= 10000);
+    int* a = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = 5;
+    }
+    queue(k, n, a);
+    delete[] a;
 }
