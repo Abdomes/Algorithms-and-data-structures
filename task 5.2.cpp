a13719ac7aba83268345770765c9fe834244fd46
Abdomes <91058164+Abdomes@users.noreply.github.com>
2021-11-08 17:37:26 +0500

    Update task 5.2.cpp

--- task 5.2.cpp
+++ task 5.2.cpp
@@ -6,116 +6,116 @@
 
 using namespace std;
 
-class Stack
+class stack
 {
 public:
-	Stack()
+    stack()
+    {
+        size = 0;
+	buffer_size = 1;
+	buffer = new char[buffer_size];
+    }
+    ~stack()
+    {
+	delete[]buffer;
+    }
+    int getsize() { return size; }
+    void grow()
+    {
+        int newbuffersize = buffer_size * 2;
+	char* newbufer = new char[newbuffersize];
+	for (int i = 0; i < buffer_size; i++)
 	{
-		size = 0;
-		buffer_size = 1;
-		buffer = new char[buffer_size];
+	    newbufer[i] = buffer[i];
 	}
-	~Stack()
+	delete[] buffer;
+	buffer = newbufer;
+	buffer_size = buffer_size * 2;
+    }
+    void push_back(char data)
+    {
+	if (size == buffer_size)
 	{
-		delete[]buffer;
+	    grow();
 	}
-	int getsize() { return size; }
-	void grow()
+	assert(size < buffer_size&& buffer_size != 0);
+	buffer[size] = data;
+	size++;
+    }
+    char pop_back()
+    {
+        if (size == 0)
 	{
-		int newbuffersize = buffer_size * 2;
-		char* newbufer = new char[newbuffersize];
-		for (int i = 0; i < buffer_size; i++)
-		{
-			newbufer[i] = buffer[i];
-		}
-		delete[] buffer;
-		buffer = newbufer;
-		buffer_size = buffer_size * 2;
+	    return '0';
 	}
-	void push_back(char data)
+	else if (size != 0)
 	{
-		if (size == buffer_size)
-		{
-			grow();
-		}
-		assert(size < buffer_size&& buffer_size != 0);
-		buffer[size] = data;
-		size++;
+	    return buffer[--size];
 	}
-	char pop_back()
+    }
+    char top_back()
+    {
+        if (size ==     0)
 	{
-		if (size == 0)
-		{
-			return '0';
-		}
-		else if (size != 0)
-		{
-			return buffer[--size];
-		}
+	    return '0';
 	}
-	char top_back()
+	else
 	{
-		if (size == 0)
-		{
-			return '0';
-		}
-		else
-		{
-			return buffer[size - 1];
-		}
+	    return buffer[size - 1];
 	}
+    }
 private:
-	char* buffer;
-	int buffer_size;
-	int size;
+    char* buffer;
+    int buffer_size;
+    int size;
 };
 
 void anagram(string& word1, string& word2)
 {
-	Stack a;
-	int j = 0;
-	for (int i = 0; i < word2.size(); i++)
+    stack a;
+    int j = 0;
+    for (int i = 0; i < word2.size(); i++)
+    {
+        for (j; j < word1.size(); )
 	{
-		for (j; j < word1.size(); )
-		{
-			if (a.top_back() == word2[i])
-			{
-				++j;
-				a.pop_back();
-				break;
-			}
-			else if (word2[i] == word1[j])
-			{
-				a.push_back(word1[j]);
-				++j;
-				a.pop_back();
-				break;
-			}
-			else
-			{
-				a.push_back(word1[j]);
-				++j;
-			}
-		}
-		if (j == word1.size() && a.top_back() == word2[i])
-		{
-			a.pop_back();
-		}
-	}
-	if (a.getsize() == 0)
+	    if (a.top_back() == word2[i])
+	    {
+                ++j;
+		a.pop_back();
+		break;
+	    }
+	    else if (word2[i] == word1[j])
+	    {
+	        a.push_back(word1[j]);
+		++j;
+		a.pop_back();
+		break;
+	    }
+	    else
+	    {
+                a.push_back(word1[j]);
+		++j;
+	    }
+        }
+	if (j == word1.size() && a.top_back() == word2[i])
 	{
-		cout << "YES";
-	}
-	else
-	{
-		cout << "NO";
-	}
+            a.pop_back();
+        }
+    }
+    if (a.getsize() == 0)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
 }
 
 int main()
 {
-	string word1, word2;
-	cin >> word1 >> word2;
-	assert(word1.size() <= 10000 && word2.size() <= 10000);
-	anagram(word1, word2);
+    string word1, word2;
+    cin >> word1 >> word2;
+    assert(word1.size() <= 10000 && word2.size() <= 10000);
+    anagram(word1, word2);
 }
