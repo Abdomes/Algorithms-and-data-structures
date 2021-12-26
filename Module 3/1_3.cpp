a2d78cc4b0170448dd10ac8465dfadc3e7cd285e
Abdomes <91058164+Abdomes@users.noreply.github.com>
2021-12-26 21:45:20 +0300

    Update 1_3.cpp

--- Module 3/1_3.cpp
+++ Module 3/1_3.cpp
@@ -89,14 +89,14 @@ public:
     }
 
     void Post_Order()
-	{
+    {
         Post_Order(root);
     }
 };
 
 int main()
 {
-	BinTree tree;
+    BinTree tree;
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
